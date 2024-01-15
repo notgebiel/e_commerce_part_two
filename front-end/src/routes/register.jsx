aa4f7e8092908f35c1c317a9d4797805dbc4b40b
notgebiel <cloostermansemile@gmail.com>
2024-01-15 20:52:51 +0100

    register form basic infrastructure

--- front-end/src/routes/register.jsx
+++ front-end/src/routes/register.jsx
@@ -1,10 +1,19 @@
+import { useState } from "react";
 import Navbar from "../components/navbar";
 
 export default function Register() {
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(JSON.stringify(inputs));
+    }
     return (
         <div>
             <Navbar active='login' />
-            <h1>register</h1>
+            <form onSubmit={handleSubmit}>
+                
+                
+            </form>
         </div>
     )
 }
